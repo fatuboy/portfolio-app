a90027db52e8b081d20656bf3d365483b68ab33a
fatuboy <81263853+fatuboy@users.noreply.github.com>
2022-03-12 15:13:08 -0600

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#delete existing nodejs file if exist
-#rm -rf /home/ec2-user/nodejs
-mkdir /home/ec2-user/nodejs
-cd /home/ec2-user/nodejs
 
-sudo npm install
+# Stop all servers and start the server as a daemon
+forever stopall
+# forever start --minUptime 10000 --spinSleepTime 1000 /home/ec2-user/nodejs/app.js
+sudo forever start -c "npm start" /home/ubuntu/myportfolio/ --minUptime 10000 --spinSleepTime 1000
