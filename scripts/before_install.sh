629fe31b7fe5a9cf334b91e1b25ecca1c981b2be
Ayo <faturotty@hotmail.com>
2022-03-12 18:17:39 -0600

    update scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install node.js  
+<<<<<<< Updated upstream
 sudo apt-get update
 sudo apt-get install gcc g++ make
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
@@ -11,6 +12,12 @@ sudo npm install -g forever
 # sudo npm audit fix --force
 
 
+=======
+sudo apt-get install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get update
+sudo apt-get install -y nodejs
+>>>>>>> Stashed changes
 
 
 
